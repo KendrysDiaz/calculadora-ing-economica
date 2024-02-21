d2700a87071c1ebc4b1c6d1c942b8ae196d79581
KendrysDiaz <kendrys333@gmail.com>
2024-02-20 19:57:06 -0500

    3 Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-green': '#04773B',
+      }
+    },
   },
   plugins: [],
 };
