02efc1329a7d1496a7b6d957994189f5c5b4a657
KendrysDiaz <kendrys333@gmail.com>
2024-02-20 19:00:59 -0500

    2 Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /**  @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src//*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [],
 };
 
