ece950e64a2023ee995b2136931a72edd0b73f17
KendrysDiaz <kendrys333@gmail.com>
2024-02-22 20:23:09 -0500

    Team home

---
+++ src/app/shared/teamwork/teamwork.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teamwork',
+  standalone: true,
+  imports: [],
+  templateUrl: './teamwork.component.html',
+  styleUrl: './teamwork.component.css'
+})
+export class TeamworkComponent {
+
+}
