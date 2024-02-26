a08ac264ab778907e0b08ef0261d77da24b706ab
KendrysDiaz <kendrys333@gmail.com>
2024-02-26 13:30:45 -0500

    Arreglo de inputs

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,35 +1,21 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
-
 @Component({
   selector: 'app-input',
   standalone: true,
   imports: [],
-  template: `<div class="flex gap-4 justify-between mb-3">
-    <h3 class="text-base text-black font-semibold">{{ title }}</h3>
-    <input
-      class="input w-9/12"
-      [value]="info"
-      (change)="onSelect($event)"
-      type="number"
-    />
-  </div> `,
+  templateUrl: './input.component.html',
   styleUrl: './input.component.css',
 })
 export class InputComponent {
   @Input() title!: string;
   @Input() info!: string;
-  @Output() infoChange = new EventEmitter<string>();
-  @Input() control!: FormControl;
+  @Input() type!: string;
 
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    console.log('titulo:', this.title);
-  }
+  valor: any;
 
-  onSelect(event: any) {
-    const selectedValue = event.target.value;
-    this.infoChange.emit(selectedValue);
+  @Output() valorChange = new EventEmitter<any>();
+
+  onValorChange() {
+    this.valorChange.emit(this.valor);
   }
 }
