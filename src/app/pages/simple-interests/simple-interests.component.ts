c214f3222634e36ad3d4fdfe05f22b33c0192f92
WilcarOrtiz <ortizcolpaswilcardaniel@gmail.com>
2024-03-04 10:39:40 -0500

    cambios para que funcione bien el timepo

--- src/app/pages/simple-interests/simple-interests.component.ts
+++ src/app/pages/simple-interests/simple-interests.component.ts
@@ -12,21 +12,20 @@ import { TimeCheckComponent } from '../../shared/time-check/time-check.component
 export default class SimpleInterestsComponent {
   @ViewChild(TimeCheckComponent) hijo!: TimeCheckComponent;
   time: any;
-  capital:any;
-  interes:any;
-  monto:any;
-  tasainteres:any;
+  capital: any;
+  interes: any;
+  monto: any;
+  tasainteres: any;
   onValorChange1(value: any) {
     this.capital = value;
   }
   onValorChange2(value: any) {
     if (value.includes('%')) {
       let [numero, porcentaje] = value.split('%');
-      this.tasainteres = numero/100
+      this.tasainteres = numero / 100;
     } else {
       this.tasainteres = value;
     }
-
   }
   onValorChange3(value: any) {
     this.interes = value;
@@ -39,30 +38,33 @@ export default class SimpleInterestsComponent {
     this.hijo.timeCalculator();
     this.time = this.hijo.time;
   }
-  
-  intereSimpleCalculator(){
+
+  intereSimpleCalculator() {
     if (this.capital > 0 && this.tasainteres > 0 && this.time > 0) {
-      this.interes = this.capital * this.tasainteres * this.time
-      this.monto = this.capital * (1 + (this.tasainteres * this.time))
+      this.interes = this.capital * this.tasainteres * this.time;
+      this.monto = this.capital * (1 + this.tasainteres * this.time);
     } else {
       if (this.monto > 0 && this.capital > 0 && this.time > 0) {
-        this.tasainteres = ((this.monto/this.capital)-1)/this.time
+        this.tasainteres = (this.monto / this.capital - 1) / this.time;
       } else {
         if (this.interes > 0 && this.capital > 0 && this.time > 0) {
-          this.tasainteres = (this.interes/(this.capital*this.time))
+          this.tasainteres = this.interes / (this.capital * this.time);
         } else {
           if (this.tasainteres > 0 && this.interes > 0 && this.time > 0) {
-            this.capital = (this.interes/(this.tasainteres * this.time))
+            this.capital = this.interes / (this.tasainteres * this.time);
           } else {
-            if (this.tasainteres>0 && this.monto>0 && this.time >0 ) {
-              this.capital = this.monto*(1+(this.tasainteres*this.time))
+            if (this.tasainteres > 0 && this.monto > 0 && this.time > 0) {
+              this.capital = this.monto * (1 + this.tasainteres * this.time);
             } else {
-              if (this.monto>0  && this.capital>0  && this.tasainteres>0 ) {
-                this.time = ((this.monto/this.capital)-1)/this.tasainteres
-              }
-              else {
-                if (this.interes>0  && this.capital>0  && this.tasainteres>0 ) {
-                  this.time = this.interes/(this.capital*this.tasainteres)
+              if (this.monto > 0 && this.capital > 0 && this.tasainteres > 0) {
+                this.time = (this.monto / this.capital - 1) / this.tasainteres;
+              } else {
+                if (
+                  this.interes > 0 &&
+                  this.capital > 0 &&
+                  this.tasainteres > 0
+                ) {
+                  this.time = this.interes / (this.capital * this.tasainteres);
                 }
               }
             }
